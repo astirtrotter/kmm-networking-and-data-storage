fd47b9a9bafcb23322d82203b984fb0c5d0f244d
astirtrotter <astirtrotter@gmail.com>
2020-12-20 00:50:53 +0800

    Minor update

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -52,14 +52,14 @@ struct ContentView: View {
 
         private func listView() -> AnyView {
             switch viewModel.launches {
-            case .loading:
-                return AnyView(Text("Loading...").multilineTextAlignment(.center))
-            case .result(let launches):
-                return AnyView(List(launches) { launch in
-                    RocketLaunchRow(rocketLaunch: launch)
-                })
-            case .error(let description):
-                return AnyView(Text(description).multilineTextAlignment(.center))
+                case .loading:
+                    return AnyView(Text("Loading...").multilineTextAlignment(.center))
+                case .result(let launches):
+                    return AnyView(List(launches) { launch in
+                        RocketLaunchRow(rocketLaunch: launch)
+                    })
+                case .error(let description):
+                    return AnyView(Text(description).multilineTextAlignment(.center))
             }
         }
 }
