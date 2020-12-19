4aefea9b828c6d975a27c10c0644de7716ca256c
astirtrotter <astirtrotter@gmail.com>
2020-12-20 00:31:24 +0800

    Implemented step 9 (Creating the iOS application)

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,18 +1,102 @@
 import SwiftUI
 import shared
 
-func greet() -> String {
-    return Greeting().greeting()
+
+struct RocketLaunchRow: View {
+    var rocketLaunch: RocketLaunch
+
+    var body: some View {
+        HStack() {
+            VStack(alignment: .leading, spacing: 10.0) {
+                Text("Launch name: \(rocketLaunch.missionName)")
+                Text(launchText).foregroundColor(launchColor)
+                Text("Launch year: \(String(rocketLaunch.launchYear))")
+                Text("Launch details: \(rocketLaunch.details ?? "")")
+            }
+            Spacer()
+        }
+    }
+}
+
+extension RocketLaunchRow {
+   private var launchText: String {
+       if let isSuccess = rocketLaunch.launchSuccess {
+           return isSuccess.boolValue ? "Successful" : "Unsuccessful"
+       } else {
+           return "No data"
+       }
+   }
+
+   private var launchColor: Color {
+       if let isSuccess = rocketLaunch.launchSuccess {
+           return isSuccess.boolValue ? Color.green : Color.red
+       } else {
+           return Color.gray
+       }
+   }
 }
 
 struct ContentView: View {
-    var body: some View {
-        Text(greet())
+    @ObservedObject private(set) var viewModel: ViewModel
+
+        var body: some View {
+            NavigationView {
+                listView()
+                .navigationBarTitle("SpaceX Launches")
+                .navigationBarItems(trailing:
+                    Button("Reload") {
+                        self.viewModel.loadLaunches(forceReload: true)
+                })
+            }
+        }
+
+        private func listView() -> AnyView {
+            switch viewModel.launches {
+            case .loading:
+                return AnyView(Text("Loading...").multilineTextAlignment(.center))
+            case .result(let launches):
+                return AnyView(List(launches) { launch in
+                    RocketLaunchRow(rocketLaunch: launch)
+                })
+            case .error(let description):
+                return AnyView(Text(description).multilineTextAlignment(.center))
+            }
+        }
+}
+
+extension ContentView {
+    enum LoadableLaunches {
+        case loading
+        case result([RocketLaunch])
+        case error(String)
+    }
+    
+    class ViewModel: ObservableObject {
+        let sdk: SpaceXSDK
+        @Published var launches = LoadableLaunches.loading
+
+        init(sdk: SpaceXSDK) {
+            self.sdk = sdk
+            self.loadLaunches(forceReload: false)
+        }
+
+        func loadLaunches(forceReload: Bool) {
+            self.launches = .loading
+            sdk.getLaunches(forceReload: forceReload, completionHandler: { launches, error in
+                if let launches = launches {
+                    self.launches = .result(launches)
+                } else {
+                    self.launches = .error(error?.localizedDescription ?? "error")
+                }
+            })
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+extension RocketLaunch: Identifiable { }
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
