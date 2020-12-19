411c3c960e0154c5509d2f16a75638d23f3262e2
astirtrotter <astirtrotter@gmail.com>
2020-12-19 22:34:05 +0800

    Implemented step 3 (adding dependencies to the multiplatform library)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,12 @@ buildscript {
         google()
         mavenCentral()
     }
+    val kotlinVersion = "1.4.10"
+    val sqlDelightVersion: String by project
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
         classpath("com.android.tools.build:gradle:4.1.1")
     }
 }
